3e30c461fd8adf876f4366737869d0d05b3b01b7
Jongmin Kim <kimjmin@gmail.com>
2014-08-15 00:48:39 +0900

    Key 가져오는 기능 추가.

--- JmUtil/src/jm/net/DataEntity.java
+++ JmUtil/src/jm/net/DataEntity.java
@@ -1,6 +1,9 @@
 package jm.net;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.Map.Entry;
 
 public class DataEntity extends HashMap<String, Object>{
 	
@@ -8,6 +11,16 @@ public class DataEntity extends HashMap<String, Object>{
 
 	public int getLength(){
 		return this.size();
+	}
+	
+	public String[] getKeys(){
+		Iterator iterator = this.entrySet().iterator();
+		Vector<String> keys = new Vector<String>();
+		while (iterator.hasNext()) {
+			Entry entry = (Entry)iterator.next();
+			keys.add(entry.getKey()+"");
+		}
+		return keys.toArray(new String[keys.size()]);
 	}
 	
 }
